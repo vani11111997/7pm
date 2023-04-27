93d9e61101797d41aee8b705eb70283175f74b4c
vani11111997 <119415689+vani11111997@users.noreply.github.com>
2023-04-27 11:09:15 +0530

    abc commits

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAU62M2BJSFFVFHZ2D"
+  secret_key = "4e3wHPditgvL0xu99B2jn6uimQ7XAPPZp2u6iraw"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0e6329e222e662a52"
+  instance_type = "t2.micro"
+  key_name = "vanikops"
+  availabilty_zone = "ap-south-1b"
+  tags= {
+    Name = "vani terraform"
+  }
+}
